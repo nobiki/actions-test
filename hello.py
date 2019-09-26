9fb3580ed86ffdbc9f9300bf32cc53ca18264c55
Naoaki Obiki <obiki.wk@gmail.com>
2019-09-26 15:50:52 +0900

    update

--- hello.py
+++ hello.py
@@ -4,21 +4,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-# this is sample program
-print("Hello")
-
-# grid_host = os.environ.get('GRID_HOST')
-
-if "chrome" == os.environ.get('BROWSER'):
-    browser=DesiredCapabilities.CHROME
-elif "edge" == os.environ.get('BROWSER'):
-    browser=DesiredCapabilities.EDGE
-elif "ie" == os.environ.get('BROWSER'):
-    browser=DesiredCapabilities.INTERNETEXPLORER
-else:
-    browser=DesiredCapabilities.FIREFOX
-
-driver = webdriver.Ie(desired_capabilities=browser)
+driver = webdriver.Ie("Selenium.WebDriver.IEDriver.3.150.0")
 driver.get("https://7me.oji.0j0.jp")
 time.sleep(10)
 driver.close()
