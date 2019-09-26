4b633b1f429a95f9dbe7b5c56c7d6cd591448f4b
Naoaki Obiki <obiki.wk@gmail.com>
2019-09-26 16:12:32 +0900

    update

--- hello.py
+++ hello.py
@@ -6,6 +6,6 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 driver = webdriver.Ie("Selenium.WebDriver.IEDriver.3.150.0\driver\IEDriverServer.exe")
 driver.get("https://7me.oji.0j0.jp")
-time.sleep(10)
+print(driver.title)
 driver.close()
 driver.quit()
