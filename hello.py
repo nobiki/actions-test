9b25a85ea7edc63a9c2c2663c8946d4fbcf11c58
Naoaki Obiki <obiki.wk@gmail.com>
2019-09-26 16:00:05 +0900

    update

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-driver = webdriver.Ie("Selenium.WebDriver.IEDriver.3.150.0")
+driver = webdriver.Ie("IEDriverServer.exe")
 driver.get("https://7me.oji.0j0.jp")
 time.sleep(10)
 driver.close()
